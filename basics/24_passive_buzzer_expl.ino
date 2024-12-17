18ccba97217435ad304ca63c0fe050061f6549de
Sofia K <sophiakirpichenok@gmail.com>
2024-12-17 21:22:17 +0100

    completed lecture 24

---
+++ basics/24_passive_buzzer_expl.ino
@@ -0,0 +1,13 @@
+const int BUZZER_PIN = 8;
+const int BUZZER_TIME = 1000;
+
+void setup() {
+  pinMode(BUZZER_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(BUZZER_PIN, HIGH);
+  delayMicroseconds(BUZZER_TIME);
+  digitalWrite(BUZZER_PIN, LOW);
+  delayMicroseconds(BUZZER_TIME);
+}
